bd3573c03118d8d8aec9919920d89dd754d6709b
Jerrinald <jerrinald95190@live.fr>
2024-06-26 16:50:42 +0200

    message design

--- src/app/ui/Header.tsx
+++ src/app/ui/Header.tsx
@@ -1,8 +1,7 @@
 "use client";
 
 import Image from "next/image";
-import Link from "next/link";
-import { usePathname } from "next/navigation";
+import NavLink from "./NavLink";
 import {
   FaCaretDown,
   FaChartLine,
@@ -16,8 +15,6 @@ import {
 } from "react-icons/fa";
 
 export default function Header() {
-  const currentPath = usePathname();
-
   return (
     <header className="flex flex-col bg-black text-white text-sm">
       <Image
@@ -29,56 +26,16 @@ export default function Header() {
       />
       <nav className="flex items-center justify-between gap-6 h-16 border-y-2 border-gray-600">
         <div className="flex flex-row ml-14 gap-7 h-full">
-          <Link
-            className={`flex items-center gap-2 cursor-pointer border-b-2 transition duration-300 ${currentPath === "/" ? "text-orange-500 border-orange-500" : "border-transparent hover:text-orange-500 hover:border-orange-500"}`}
-            href="/"
-          >
-            <FaUserFriends /> Tous les patients
-          </Link>
-          <Link
-            className={`flex items-center gap-2 cursor-pointer border-b-2 transition duration-300 ${currentPath === "/waiting-list" ? "text-orange-500 border-orange-500" : "border-transparent hover:text-orange-500 hover:border-orange-500"}`}
-            href="/waiting-list"
-          >
-            <FaListAlt /> Liste d&apos;attente
-          </Link>
-          <Link
-            className={`flex items-center gap-2 cursor-pointer border-b-2 transition duration-300 ${currentPath === "/report" ? "text-orange-500 border-orange-500" : "border-transparent hover:text-orange-500 hover:border-orange-500"}`}
-            href="/report"
-          >
-            <FaChartLine /> Rapport
-          </Link>
-          <Link
-            className={`flex items-center gap-2 cursor-pointer border-b-2 transition duration-300 ${currentPath === "/dashboard" ? "text-orange-500 border-orange-500" : "border-transparent hover:text-orange-500 hover:border-orange-500"}`}
-            href="/dashboard"
-          >
-            <FaTachometerAlt /> Tableau de bord
-          </Link>
-          <Link
-            className={`flex items-center gap-2 cursor-pointer border-b-2 transition duration-300 ${currentPath === "/statistics" ? "text-orange-500 border-orange-500" : "border-transparent hover:text-orange-500 hover:border-orange-500"}`}
-            href="/statistics"
-          >
-            <FaChartPie /> Statistiques
-          </Link>
+          <NavLink text="Tous les patients" href="/" icons={[<FaUserFriends />]} />
+          <NavLink text="Liste d'attente" href="/waiting-list" icons={[<FaListAlt />]} />
+          <NavLink text="Rapport" href="/report" icons={[<FaChartLine />]} />
+          <NavLink text="Tableau de bord" href="/dashboard" icons={[<FaTachometerAlt />]} />
+          <NavLink text="Statistiques" href="/statistics" icons={[<FaChartPie />]} />
         </div>
         <div className="flex flex-row mr-3 gap-7 h-full">
-          <Link
-            className={`flex items-center gap-2 cursor-pointer border-b-2 transition duration-300 ${currentPath === "/authorization" ? "text-orange-500 border-orange-500" : "border-transparent hover:text-orange-500 hover:border-orange-500"}`}
-            href="/authorization"
-          >
-            <FaLock /> Autorisation
-          </Link>
-          <Link
-            className={`flex items-center gap-1 cursor-pointer border-b-2 transition duration-300 ${currentPath === "/help" ? "text-orange-500 border-orange-500" : "border-transparent hover:text-orange-500 hover:border-orange-500"}`}
-            href="/help"
-          >
-            <FaQuestionCircle /> Aide <FaCaretDown />
-          </Link>
-          <Link
-            className={`flex items-center gap-1 cursor-pointer border-b-2 transition duration-300 ${currentPath === "/settings" ? "text-orange-500 border-orange-500" : "border-transparent hover:text-orange-500 hover:border-orange-500"}`}
-            href="/settings"
-          >
-            <FaCog /> <FaCaretDown />
-          </Link>
+          <NavLink text="Autorisation" href="/authorization" icons={[<FaLock />]} />
+          <NavLink text="" href="/help" icons={[<FaQuestionCircle />, <FaCaretDown />]} />
+          <NavLink text="" href="/settings" icons={[<FaCog />, <FaCaretDown />]} />
         </div>
       </nav>
     </header>
