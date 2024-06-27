0ec42f6a20080d7e9b30c2dd33ab3a34c0b2839d
Théo Hervé <theojherve@gmail.com>
2024-06-27 14:25:10 +0200

    :sparkles: Tour de Théo qui fonctionne promis (#9)
    :sparkles: Tour de Théo qui fonctionne promis

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
 "use client";
-
+import React, {useRef, useState} from 'react';
+import {Modal, Table, Tour} from 'antd';
+import type { TourProps } from 'antd';
+import {ArrowUpOutlined, QuestionCircleOutlined} from "@ant-design/icons";
 import { useQuery } from "@tanstack/react-query";
-import { Table } from "antd";
 import { userService } from "../../services/user";
 import PatientMessages from "./Components/PatientMessage";
 import PatientState from "./Components/PatientState";
@@ -11,17 +13,73 @@ const App = () => {
     queryKey: ["users"],
     queryFn: userService.fetchAll,
   });
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const [openTuto, setOpenTuto] = useState<boolean>(false);
+  const ref1 = useRef(null);
+  const ref2 = useRef(null);
+  const [isTourAtStep3, setIsTourAtStep3] = useState(false);
+
+  const steps: TourProps['steps'] = [
+    {
+      title: 'Tutoriel',
+      description: 'La colonne "Catégorie" permet de visualiser rapidement l\'état du patient.',
+      placement: 'right',
+      target: () => ref1.current,
+    },
+    {
+      title: 'La catégorie',
+      description: 'Le code couleur correspond à une catégorie de problème. Au click, une modale s\'ouvre avec plus de détails.',
+      placement: 'right',
+      target: () => ref2.current,
+    },
+    {
+      title: 'La modal',
+      description: 'Dans cette modal, vous pouvez voir le statut du patient et des informations supplémentaires comme des conseils pour aider au mieux le patient.',
+      placement: 'bottom',
+      target: null,
+      cover: (
+        <ArrowUpOutlined className="absolute -top-8" />
+      )
+    },
+  ];
+
+  const handleStepChange = (current: number) => {
+    if (current === 2) {
+      showModal();
+      setIsTourAtStep3(true);
+    } else {
+      setIsTourAtStep3(false);
+    }
+  };
+
+  const showModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleOk = () => {
+    setIsModalOpen(false);
+  };
+
+  const handleCancel = () => {
+    setIsModalOpen(false);
+  };
 
   const columns = [
     {
-      title: "Pastille",
-      dataIndex: "state",
-      key: "state",
-      render: (text: string, record: any) => (
-        <PatientState text={text} record={record} />
+      title: () => (
+        <div className="flex items-center gap-2" ref={ref1}>
+          <div>Catégorie</div>
+          <QuestionCircleOutlined onClick={() => setOpenTuto(true)} className="cursor-pointer" />
+        </div>
       ),
+      dataIndex: 'state',
+      key: 'state',
+      render: (text: string, record: any, index: number) => (
+        <PatientState text={text} record={record} index={index} ref={index === 0 ? ref2 : null} />
+      ),
+      align: 'center',
     },
-    { title: "État", dataIndex: "state", key: "state" },
     { title: "Protocole", dataIndex: "protocole", key: "protocole" },
     { title: "Téléphone", dataIndex: "phoneNumber", key: "phoneNumber" },
     { title: "Suivi SMS", dataIndex: "sms", key: "sms" },
@@ -73,9 +131,42 @@ const App = () => {
   }
 
   return (
-    <div>
-      <Table columns={columns} dataSource={data} />
-    </div>
+    <>
+      <div className="h-dvh">
+        <div className="px-4">
+          <Table columns={columns} dataSource={data}/>
+        </div>
+      </div>
+
+      <Tour
+        open={openTuto}
+        onClose={() => {
+          setOpenTuto(false);
+          setIsTourAtStep3(false);
+        }}
+        steps={steps}
+        onChange={handleStepChange}
+        indicatorsRender={(current, total) => (
+          <span>
+            {current + 1} / {total}
+          </span>
+        )}
+        mask={!isTourAtStep3}
+        type={ isTourAtStep3 ? 'primary' : 'neutral'}
+      />
+
+      <Modal title="Patient status" open={isModalOpen} onOk={handleOk} onCancel={handleCancel}>
+        <div className="flex flex-col">
+          <h1 className="text-lg font-bold">Catégorie</h1>
+
+          <p className="mt-2 font-bold">État du patient</p>
+          <p className="text-justify">Ici se trouve un cours résumé de l&apos;état de santé du patient</p>
+
+          <p className="mt-2 font-bold">Conseil</p>
+          <p className="text-justify">Dans cette partie, notre intélligence artificielle génère une courte synthèse avec une aide de comment aider au mieux le patient.</p>
+        </div>
+      </Modal>
+    </>
   );
 };
 
